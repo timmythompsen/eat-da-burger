a26f9e915de52f63251e18cf60b6822feb999355
Timothy Thompsen <TimmyThompsen@Timothys-MacBook-Pro.local>
2017-11-11 08:48:04 -0800

    commit to github

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Spicy Jalepeno Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Ranch Chicken Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Cheeseburger', true);
